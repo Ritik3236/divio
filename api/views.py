306784d1b899616720cdd27170b76ebd65dc7a14
Ritik3236 <ritikkushwaha1234@gmail.com>
2021-05-31 15:10:41 +0530

    ..

---
+++ api/views.py
@@ -0,0 +1,59 @@
+from django.views.generic import TemplateView
+from django.http import JsonResponse
+from django.core import serializers
+from django.shortcuts import render
+
+from api.models import *
+from api.forms import *
+
+import random
+# Create your views here.
+
+def quote_record(request):
+    image_url = []
+    q = list(QuotesApi.objects.all())
+    rq = random.sample(q, 4) if len(q) > 4 else q
+    for i in rq:
+        try:
+            image_url.append(i.author_img.url)
+        except Exception as e:
+            image_url.append('img not found')
+    randomized_quote = serializers.serialize('json', rq)
+    context = {
+        'data': randomized_quote,
+        'image_url': image_url
+    }
+    return JsonResponse(
+        {'context': context}, status=200
+    )
+
+
+class TestView(TemplateView):
+    template_name = 'upload.html'
+
+    def get(self, request, *args, **kwargs):
+        context = {'form': TestForm()}
+        return render(request, self.template_name, context)
+
+    def post(self, request, count=0, *args, **kwargs):
+        file_upload = FileUpload()
+        print(count)
+        upload_result = ''
+        form = TestForm(request.POST, request.FILES)
+        files = request.FILES.getlist('document')
+        if form.is_valid():
+            for file in files:
+                file_upload.name = form.cleaned_data['name']
+                file_upload.email = form.cleaned_data['email']
+                file_upload.type = form.cleaned_data['type']
+                file_upload.document = file
+                file_upload.description = form.cleaned_data['description']
+                file_upload.save()
+                count = count + 1
+            upload_result = 'Upload Successful'
+        context = {
+            'form': TestForm(),
+            'upload_result': upload_result,
+            'count': count,
+        }
+        return render(request, self.template_name, context)
---
+++ home_page/views.py
@@ -0,0 +1,84 @@
+# from django.db.models.base import Model
+from django.shortcuts import render
+from django.views.generic import TemplateView
+
+from home_page.models import *
+
+
+def sort_by_values(dic=None):
+    if dic:
+        return {k: v for k, v in sorted(dic.items(), key=lambda item: item[1])}
+
+
+def txt_to_list(txt, spacer=' '):
+    return txt.split(spacer)
+
+
+def func_course_list():
+    context = {'1' : 'nodata' }
+    if Course.objects.all().exists():
+        context={}
+        for obj in Course.objects.all().iterator():
+            context.update({str(obj.id): obj.c_name})
+        return sort_by_values(context).items()
+    return (context)
+
+
+# Create your views here.
+
+
+class HomePageView(TemplateView):
+    template_name = 'home_page.html'
+
+    def get(self, request, *arg, **kwargs):
+        sem_list = {'1': '1st', '2': '2nd', '3': '3rd',
+                    '4': '4th', '5': '5th', '6': '6th'}
+        context = {
+            'course_list': func_course_list(),
+            'sem_list': sem_list,
+            'sub_list': txt_to_list('Plz select Course & semester 👈'),
+            'questions': ['Plz Select Any Subject 📚 '],
+        }
+        return render(request, self.template_name, context)
+
+
+class QuestionView(TemplateView):
+    template_name = 'home_page.html'
+
+    def get(self, request, sem_id=1, sub_name='Hindi', *args, **kwargs):
+
+        questions = []
+        ques_list = []
+        sem_list = {'1': '1st', '2': '2nd', '3': '3rd',
+                    '4': '4th', '5': '5th', '6': '6th'}
+
+        if Subject.objects.filter(course_name_id__pk=kwargs['c_id']).exists():
+            s = Subject.objects.get(course_name_id__pk=kwargs['c_id'])
+            sub_list = list(s.sub_names.split(","))
+            ques_result = QuesPaper.objects.filter(
+                course_name=kwargs['c_id'], semester=sem_id, sub_name=sub_name.lower())
+            for q in ques_result:
+                ques_list = list(q.fl_id.split(","))
+            for i in ques_list:
+                try:
+                    q_paper = QuesPaperMedia.objects.get(fl_id=i.lower())
+                    questions.append(q_paper)
+                except QuesPaperMedia.DoesNotExist:
+                    q_paper = 'None'
+        else:
+            sub_list = txt_to_list('Sorry We Got No Subjects 😔')
+
+        if not questions:
+            questions = ['Question Paper Missing']
+
+        context = {
+            'course_list': func_course_list(),
+            'id': kwargs['c_id'],
+            'sem_id': sem_id,
+            'sem_list': sem_list,
+            'sub_list': sub_list,
+            'sub_name': sub_name,
+            'questions': questions,
+        }
+
+        return render(request, self.template_name, context)
